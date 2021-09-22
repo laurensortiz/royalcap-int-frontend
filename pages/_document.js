15f1edf403d39ba8c5f71b495e073e3dacf1be73
Laurens OB <laurens.ortiz@gmail.com>
2021-09-22 17:14:58 -0600

    Changes on footer and lead form

--- pages/_document.js
+++ pages/_document.js
@@ -5,25 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MZJCMBR');`,
-            }}
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: ` window.callbellSettings = {
-            token: "ntPvbmqv9ktEAV5Qn24cREbK"
-          };
-                      (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-T2KR297VKX"></script>
 
-          `,
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-T2KR297VKX');`,
             }}
-          ></script>
+          />
 
           <link
             rel="stylesheet"
@@ -37,18 +29,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=GTM-MZJCMBR"
-                  height="0"
-                  width="0"
-                  style="display:none;visibility:hidden"
-                ></iframe>
-              `,
-            }}
-          ></noscript>
           <Main />
           <NextScript />
         </body>
