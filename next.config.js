880b1b9ed48ecffea0eb3a524b3d006664491753
Laurens OB <laurens.ortiz@gmail.com>
2024-06-01 01:47:13 -0600

    fix issue

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ module.exports = {
     locales: ['en', 'es'],
     defaultLocale: 'en',
     domains: [
-      {
-        domain: 'by.royalcap-int.com',
-        defaultLocale: 'ru',
-        http: true,
-      },
-      {
-        domain: 'royalcap-int.by',
-        defaultLocale: 'ru',
-        http: true,
-      },
-      {
-        domain: 'www.royalcap-int.by',
-        defaultLocale: 'ru',
-        http: true,
-      },
+      // {
+      //   domain: 'by.royalcap-int.com',
+      //   defaultLocale: 'ru',
+      //   http: true,
+      // },
+      // {
+      //   domain: 'royalcap-int.by',
+      //   defaultLocale: 'ru',
+      //   http: true,
+      // },
+      // {
+      //   domain: 'www.royalcap-int.by',
+      //   defaultLocale: 'ru',
+      //   http: true,
+      // },
     ],
   },
   async redirects() {
